97a233ba350be9b0a5962b6ee630667a39ba9e29
rishi1717 <rshrrrss@gmail.com>
2022-04-04 21:02:10 +0530

    users updating doe

--- routes/register.js
+++ routes/register.js
@@ -2,10 +2,12 @@ const express = require("express")
 const router = express.Router()
 const { Users, validate } = require("../mongo/userModel")
 const bcrypt = require("bcrypt")
+const joi = require("joi")
 require("dotenv").config()
 
 router.post("/", async (req, res) => {
 	try {
+		console.log(req.body)
 		const { error } = validate(req.body)
 		if (error)
 			return res.status(400).send({ message: error.details[0].message })
@@ -24,5 +26,35 @@ router.post("/", async (req, res) => {
 		res.status(500).json({ message: "Internal server error" })
 	}
 })
+
+router.put("/", async (req, res) => {
+	try {
+		const { error } = updateValidate({
+			name: req.body.name,
+			email: req.body.email,
+			phone: req.body.phone.toString(),
+		})
+		if (error)
+			return res.status(400).send({ message: error.details[0].message })
+		await Users.updateOne(
+			{ _id: req.body._id },
+			{ name: req.body.name, email: req.body.email, phone: req.body.phone }
+		)
+		const users = await Users.find()
+		res.status(201).send({users, message: "User Updated succesfully" })
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).json({ message: "Internal server error" })
+	}
+})
+
+const updateValidate = (data) => {
+	const schema = joi.object({
+		name: joi.string().required().label("name"),
+		email: joi.string().required().label("email"),
+		phone: joi.string().label("phone"),
+	})
+	return schema.validate(data)
+}
 
 module.exports = router
